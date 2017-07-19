96a95495a38cf822b4078c6a8d3314071b00ed51
ali-timi <mralitimsar@gmail.com>
2017-07-19 04:42:39 +0430

    ;D INSTALL FILE

---
+++ install.sh
@@ -0,0 +1,12 @@
+
+sudo apt install default-jre -y
+sudo apt install openjdk-8-jre-headless -y
+git clone https://github.com/ali-timi/java_app.git
+
+RED='\033[0;31m'
+
+NC='\033[0m'
+
+CYAN='\033[0;36m'
+
+echo -e "-${RED} App source installed ! ${NC}Now run app by ${CYAN}java CalcRun ${NC}command"
